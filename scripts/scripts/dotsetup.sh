1e9a37c22ca7ca57e50b38ffb321d7e08db59b7d
Stefan <master2005pop@gmail.com>
2025-05-10 01:27:58 +0200

    Fix, hopefully final

--- scripts/scripts/dotsetup.sh
+++ scripts/scripts/dotsetup.sh
@@ -28,3 +28,8 @@ cd ../..
 stow --adopt --ignore="README.md" */
 # Restores the original symlinks previously overwritten by --adopt
 git restore .
+
+# Fix packer
+cd $HOME
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
