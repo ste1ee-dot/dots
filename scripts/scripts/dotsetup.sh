55a2541ce450a321cf7248233095ae5b3a6c0714
Stefan <master2005pop@gmail.com>
2025-05-15 15:21:47 +0200

    Added a calculator app

--- scripts/scripts/dotsetup.sh
+++ scripts/scripts/dotsetup.sh
@@ -16,7 +16,8 @@ cd $HOME
 # Time to install some apps
 sudo pacman -Syu --needed --noconfirm firefox stow waybar hyprpaper nerd-fonts \
                         ttf-jetbrains-mono-nerd pipewire wireplumber go npm \
-                        pavucontrol blueman brightnessctl discord thermald
+                        pavucontrol blueman brightnessctl discord thermald \
+                        qalculate-qt
 paru -Syu --needed --noconfirm wezterm-git neovim-nightly stremio wlogout \
                         auto-cpufreq
 
